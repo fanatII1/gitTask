ce1e35c1ec51452a9d6dbb3d6bcf0b7d4c20f913
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-04-03 15:47:47 +0200

    displayed some user input

--- task4-1_project/helloWorld.js
+++ task4-1_project/helloWorld.js
@@ -2,3 +2,8 @@ document.write("Hello World");
 
 //adding the changes to the file that we have intitially createds
 console.log("This is the second change that i am going to makes");
+
+
+//modify the input so that it can show whatever the input was
+let user =  prompt("Enter a number");
+console.log(user);
